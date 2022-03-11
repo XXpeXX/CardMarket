364565aee63432fd57ac9d4e491997bcc2f10c71
Pedro <RedBoy2124@gmail.com>
2022-03-11 16:48:47 +0100

    MIgrations

--- database/migrations/2022_03_11_144750_create_sales_table.php
+++ database/migrations/2022_03_11_144750_create_sales_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCollectionsTable extends Migration
+class CreateSalesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateCollectionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('collections', function (Blueprint $table) {
+        Schema::create('sales', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('image');
-            $table->string('edition date');
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateCollectionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('collections');
+        Schema::dropIfExists('sales');
     }
 }
